38009360189e4291a0920ab4f864cbf7d405ecb0
Jeremy Kaplan <jdkaplan@metagram.net>
2021-03-06 00:43:14 -0800

    Draw a square

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use nannou::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    nannou::app(model).update(update).simple_window(view).run();
+}
+
+struct Model {}
+
+fn model(_app: &App) -> Model {
+    Model {}
+}
+
+fn update(_app: &App, _model: &mut Model, _update: Update) {}
+
+fn view(app: &App, _model: &Model, frame: Frame) {
+    let draw = app.draw();
+    draw.background().color(BLACK);
+    draw.rect().color(WHITE);
+    draw.to_frame(app, &frame).unwrap();
 }
